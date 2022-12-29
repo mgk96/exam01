1c05df293ffe4353c652ca437b5d6393c44884ad
kmg <mkkim1196@gmail.com>
2022-12-29 10:51:49 +0900

    두번쨰 커밋

--- src/exam/Hello.java
+++ src/exam/Hello.java
@@ -3,8 +3,8 @@ package exam;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		System.out.println("Hello1");
+		System.out.println("Hello2");
 	}
 
 }
