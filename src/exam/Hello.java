b462926f90f4d2c85b45b3fe7d2d7e38115c3885
kmg <mkkim1196@gmail.com>
2022-12-29 10:57:25 +0900

    세번쨰..

--- src/exam/Hello.java
+++ src/exam/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello1");
 		System.out.println("Hello2");
-		System.out.println("Hello Worid3");
+		System.out.println("Hello Worid3.");
 	}
 
 }
